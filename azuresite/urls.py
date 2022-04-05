ca6b6cfaa619b43fe299b0b08493c80c693c56bc
AngieBangiee <96352331+AngieBangiee@users.noreply.github.com>
2022-04-05 00:01:28 -0400

    added prices view and url to mtp app

--- azuresite/urls.py
+++ azuresite/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('prices/', include('modeltermpapers.urls', namespace='modeltermpapers')),
+    path('admin/', admin.site.urls),   
     path('', include('modeltermpapers.urls', namespace='modeltermpapers')),
 ]
+
+
+ 
+
--- modeltermpapers/urls.py
+++ modeltermpapers/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'modeltermpapers'
+
 urlpatterns = [ 
-    path('', views.home, name='home'),
-    path('prices/', views.prices, name='prices'),
-   
+    path('', views.home, name='home'), 
+    #path('', views.prices, name='prices'),
+    url(r'^(?P<name>\b[A-Za-z]\w+\b)/$', views.prices, name="prices"),
+    
 ] 
+
